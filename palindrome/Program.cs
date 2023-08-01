c0feca6c12926240abc74480aef33b58295cded9
jima-J9M1 <jimd3730@gmail.com>
2023-08-01 15:39:08 +0300

    Add palindrome file

---
+++ palindrome/Program.cs
@@ -0,0 +1,17 @@
+﻿bool palindrome(string str){
+    int left = 0;
+    int right = str.Length - 1;
+    bool flag = true;
+    while(left < right){
+        if(str[left] != str[right]){
+            flag = false;
+        }
+        left++;
+        right--;
+    }
+    return flag;
+}
+
+Console.Write("Enter a string: ");
+string str_val = Console.ReadLine();
+Console.WriteLine(palindrome(str_val));
